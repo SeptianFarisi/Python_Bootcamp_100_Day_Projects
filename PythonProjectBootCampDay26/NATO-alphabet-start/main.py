f3a6b774ce4b8ff9a602a2128b31dbd1aa757051
KaitoJoker <22572001@stmik.jayakarta.ac.id>
2025-06-12 23:31:57 +0700

    NATO phonetic update

--- PythonProjectBootCampDay26/NATO-alphabet-start/main.py
+++ PythonProjectBootCampDay26/NATO-alphabet-start/main.py
@@ -1,16 +1,18 @@
 import pandas
 
 nato_phonetic = pandas.read_csv("nato_phonetic_alphabet.csv")
-
 nato_dict = {row.letter:row.code for (index,row) in nato_phonetic.iterrows()}
 game = True
 
 while game:
-    name = input("Enter a word(type 0 to exit): ").upper()
-    if name == "0":
-        game = False
+    name = input("Enter a word(Type empty to exit): ").upper()
+    if name == "":
+        break
     else:
-        list_name = [n for n in name.replace(" ", "")]
-        nato_list = [nato_dict.get(l) for l in list_name]
-        print(nato_list)
-
+        try:
+            list_name = [n for n in name.replace(" ", "")]
+            nato_list = [nato_dict[l] for l in name]
+        except KeyError:
+            print("Sorry, only letters in the alphabet please")
+        else:
+            print(nato_list)
